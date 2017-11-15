f249481224774dbe9ac6243a9751e7e37b3c5784
oatnewguo <oatnewguo@gmail.com>
2017-11-15 11:44:25 -0800

    Terms of interest are now passed as an argument to the function tagged_bigrams, not to a Collocations object itself. Added functionality to collapse terms of interest and treat them as the same for the purpose of collocations. Refined output display and description. Added Dunning's log likelihood scores to output. Updated documentation.

--- collocations/collocations.py
+++ collocations/collocations.py
@@ -1,16 +1,16 @@
 import nltk
 from nltk.collocations import *
+from functools import partial
 
 class Collocations:
 
-    def __init__(self, file_path, terms=[], tagged_words_path=None):
+    def __init__(self, file_path, tagged_words_path=None):
         '''Creates a Collocations instance with a text and, optionally, terms of
             interest.
 
         file_path - string path to .txt input file; used to generate full
             description of results in output file, whether or not tagged_words
             is given
-        terms - string list of words of interest to be used in ngrams filters
         tagged_words_path - string path to .txt file containing string
             representation of list of tagged words in input file; saves time and
             resources on computation
@@ -35,17 +35,6 @@ class Collocations:
             self.tagged_words = ast.literal_eval(document.read())
             document.close()
 
-        #initialize terms of interest
-        self.terms = terms
-
-    def set_terms(self, terms):
-        '''Set a new list of terms of interest.
-
-        terms - string list of words of interest to be used in ngrams filters
-        '''
-
-        self.terms = terms
-
     def tagged_words_to_file(self, destination_path):
         '''Writes the current list of tagged words as a string to file.
 
@@ -56,70 +45,115 @@ class Collocations:
         words.write(str(self.tagged_words))
         words.close()
 
-    def tagged_bigrams(self, destination_path, num_bigrams=10, window_size=5,
-        freq_filter=3, bigram_filter=None):
+    def tagged_bigrams(self, destination_path, num_bigrams=100, window_size=5,
+        freq_filter=3, bigram_filter=None, terms=None, collapse_terms=False):
         '''Finds bigrams in the list tagged_words in the manner specified by the
             parameters and writes results to a text file.
 
         destination_path - string path to .txt output file
-        num_bigrams - int number of bigrams to display from among the most
-            frequent
+        num_bigrams - int number of bigrams to display from among the highest
+            scored
         window_size - int size of window within which to find bigrams
         freq_filter - int minimum number of occurrences for a bigram to count
         bigram_filter - function ngram filter for bigrams
+        terms - string list of terms of interest to use in ngrams filter
+            --- will only be used if a bigram_filter is given
+        collapse_terms - boolean that is True if collocations should be found
+            with all words in terms treated as the same, and False if
+            collocations should be found with individual words in terms
         '''
 
         #find bigrams
         bigram_measures = nltk.collocations.BigramAssocMeasures()
-        finder = BigramCollocationFinder.from_words(self.tagged_words,
-            window_size = window_size)
+
+        if collapse_terms:
+            tagged_words_collapsed = []
+            for (w,pos) in self.tagged_words:
+                if w in terms:
+                    tagged_words_collapsed.append(('___', pos))
+                else:
+                    tagged_words_collapsed.append((w, pos))
+            finder = BigramCollocationFinder.from_words(tagged_words_collapsed,
+                window_size = window_size)
+        else:
+            finder = BigramCollocationFinder.from_words(self.tagged_words,
+                window_size = window_size)
 
         #apply filters
         finder.apply_freq_filter(freq_filter)
         if not bigram_filter == None:
-            finder.apply_ngram_filter(bigram_filter)
+            if collapse_terms:
+                finder.apply_ngram_filter(partial(bigram_filter, ['___']))
+            else:
+                finder.apply_ngram_filter(partial(bigram_filter, terms))
 
         #open output file, write results, and close file
         results = open(destination_path, 'w')
-        results.write('Top %d bigram collocations in %s, with a window size of '
-            '%d. Filtered for bigrams with a minimum frequency of %d. '
-            % (num_bigrams, self.file_path, window_size, freq_filter))
+        results.write('Top {} bigram collocations in {}, with a window size of '
+            '{}. Filtered for bigrams with a minimum frequency of {}. '
+            .format(num_bigrams, self.file_path, window_size, freq_filter))
         if not bigram_filter == None:
-            results.write('Filtered for bigrams for which %s returns False. '
-                'Potentially filtered using the following terms of interest:\n'
-                % (bigram_filter.__name__))
-            for t in self.terms:
-                results.write('\t%s\n' % t)
-        results.write('Sorted according to Dunning\'s log likelihood ratio, but '
+            results.write('Filtered for bigrams for which {} returns False. '
+                'Depending on {}, potentially filtered using the following '
+                'terms of interest:\n'.format(bigram_filter.__name__,
+                bigram_filter.__name__))
+            for t in terms:
+                results.write('\t{}\n'.format(t))
+            if collapse_terms:
+                results.write('Terms of interest, represented as \"___\", were '
+                    'collapsed and treated as the same term for collocations. ')
+        results.write('Sorted according to Dunning\'s log-likelihood ratio, but '
             'displaying frequency counts.\n---------------------------------\n')
 
-        bigrams = finder.nbest(bigram_measures.likelihood_ratio, num_bigrams)
+        results.write('Rank | Log-Likelihood Score | Frequency | Collocation\n')
+        results.write('-----------------------------------------------------\n')
+        bigrams = finder.score_ngrams(bigram_measures.likelihood_ratio)
         freq_dist = finder.ngram_fd
-        for (word1, pos1), (word2, pos2) in bigrams:
-            results.write('%d %s %s \n' %
-                (freq_dist[(word1, pos1), (word2, pos2)], word1, word2))
+        count = 1
+        for ((word1, pos1), (word2, pos2)), score in bigrams:
+            results.write('{:<4}   {:<20.10}   {:<9}   {} {} \n'.format(count, score,
+                freq_dist[(word1, pos1), (word2, pos2)], word1, word2))
+            if count == num_bigrams:
+                break
+            count += 1
         results.close()
 
-    def adjective_filter(self, word1, word2):
+    def adjective_filter(self, terms, word1, word2):
         '''Returns False if one word is a term of interest and the other has
             been tagged as an adjective.
 
+        terms - string list of terms of interest
+            --- if None, then the function will not filter for terms of interest
         word1, word2 - tuples of a string word and its string pos tag
         '''
+        if terms == None:
+            if (word1[1] == 'JJ' or word2[1] == 'JJ'):
+                return False
+            else:
+                return True
+        else:
+            if (word1[0] in terms and word2[1] == 'JJ' or
+                word2[0] in terms and word1[1] == 'JJ'):
+                return False
+            else:
+                return True
 
-        if (word1[0] in self.terms and word2[1] == 'JJ' or
-            word2[0] in self.terms and word1[1] == 'JJ'):
-            return False
-        return True
-
-    def verb_filter(self, word1, word2):
+    def verb_filter(self, terms, word1, word2):
         '''Returns False if one word is a term of interest and the other has
             been tagged as a verb.
 
+        terms - string list of terms of interest
+            --- if None, then the function will not filter for terms of interest
         word1, word2 - tuples of a string word and its string pos tag
         '''
-
-        if (word1[0] in self.terms and word2[1].startswith('VB') or
-            word2[0] in self.terms and word1[1].startswith('VB')):
-            return False
-        return True
+        if terms == None:
+            if (word1[1].startswith('VB') or word2[1].startswith('VB')):
+                return False
+            else:
+                return True
+        else:
+            if (word1[0] in terms and word2[1].startswith('VB') or
+                word2[0] in terms and word1[1].startswith('VB')):
+                return False
+            else:
+                return True
--- collocations/run_collocations.py
+++ collocations/run_collocations.py
@@ -8,15 +8,15 @@ def main():
     '''Use this code in order to tag words with parts of speech and save results
         in a .txt file. Once a text file has been saved, use the code below to
         load data much faster.
-        
-    c = Collocations(file_path = 'data\\harrison1.txt', terms = terms)
-    c.tagged_words_to_file(destination_path = 'tagged_words\\harrison1.txt')
-    '''
 
-    c = Collocations(file_path = 'data\\harrison1.txt', terms = terms,
-        tagged_words_path = 'tagged_words\\harrison1.txt')
-    c.tagged_bigrams(destination_path='results\\harrison1.txt',
-        bigram_filter=c.adjective_filter)
+    c = Collocations(file_path = 'data\\harrison.txt')
+    c.tagged_words_to_file(destination_path = 'tagged_words\\harrison.txt')
+'''
+
+    c = Collocations(file_path = 'data\\harrison.txt',
+        tagged_words_path = 'tagged_words\\harrison.txt')
+    c.tagged_bigrams(destination_path='results\\harrison_verbs_collapsed.txt',
+        bigram_filter=c.verb_filter, terms=terms, collapse_terms=True)
 
 if __name__ == '__main__':
     main()

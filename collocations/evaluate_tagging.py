33c7c49b32a03ee15e22c71dda417c166abe97f2
oatnewguo <oatnewguo@gmail.com>
2017-12-12 16:59:32 -0800

    Rewrote tagging evaluation to include window of extra words for context for determining part of speech.

--- collocations/evaluate_tagging.py
+++ collocations/evaluate_tagging.py
@@ -10,17 +10,19 @@ class EvaluateTagging:
             representation of a list of tagged words
         '''
 
-        document = open(tagged_words_path, 'r')
+        document = open(tagged_words_path, 'r', encoding='utf-8')
         self.tagged_words = ast.literal_eval(document.read())
         document.close()
         self.list_length = len(self.tagged_words)
 
-    def evaluate(self, num_words, output_path):
+    def evaluate(self, num_words, output_path, window=3):
         '''With the help of the user, evaluate num_words tagged words and write
             results to file at output_path.
 
             output_path - string path to .txt file in which to write results
             num_words - int number of tagged words to evaluate
+            window - number of words to show on either side of evaluated word
+                for context
         '''
 
         print('\nYou will be evaluating the correctness of {} tagged words. For'
@@ -36,15 +38,26 @@ class EvaluateTagging:
 
         for i in range(num_words):
             answer = None
-            word, pos = self.tagged_words[randint(0, self.list_length - 1)]
+            random = randint(0, self.list_length - 1)
+            word, pos = self.tagged_words[random]
+            before = self.tagged_words[max(0, random - window) : random]
+            after = self.tagged_words[random + 1 : min(self.list_length,
+                random + window + 1)]
+            before = [word for word, pos in before]
+            after = [word for word, pos in after]
+            before = ' '.join(before)
+            after = ' '.join(after)
             while not (answer == 'y' or answer == 'n'):
-                answer = input('{} : {}   '.format(pos, word))
+                answer = input('{}   ({} : {})   {}   |   '.format(before, pos, word,
+                    after))
             if answer == 'y':
                 correct += 1
-                results += 'Y {} : {}\n'.format(pos, word)
+                results += 'Y {}   ({} : {})   {}\n'.format(before, pos, word,
+                    after)
             elif answer == 'n':
                 incorrect += 1
-                results += 'N {} : {}\n'.format(pos, word)
+                results += 'N {}   ({} : {})   {}\n'.format(before, pos, word,
+                    after)
 
         results_file = open(output_path, 'w')
         results_file.write('Number correctly tagged: {}\n'.format(correct))
@@ -57,5 +70,5 @@ class EvaluateTagging:
 
         print('You have finished evaluating {} tagged words. {} words were '
             'tagged correctly, and {} were tagged incorrectly. Tagging has an '
-            'estimated accuracy on this text of {:.3}%.'.format(num_words,
+            'estimated accuracy on this text of {}%.'.format(num_words,
             correct, incorrect, 100.0 * correct / (correct + incorrect)))
--- collocations/run_evaluation.py
+++ collocations/run_evaluation.py
@@ -3,8 +3,8 @@ from collocations import *
 from evaluate_tagging import *
 
 def main():
-    e = EvaluateTagging('tagged_words/jennings.txt')
-    e.evaluate(100, 'tagging_evaluations/jennings.txt')
+    e = EvaluateTagging('tagged_words/filson.txt')
+    e.evaluate(100, 'tagging_evaluations/filson.txt')
 
 if __name__ == '__main__':
     main()
